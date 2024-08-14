7ca572c138c830e9c9c19bcdd5dec4fe7ebbf1ac
DaviBenassiTs <davibenassiofc@gmail.com>
2024-08-14 00:02:35 -0300

    Configure who will get the connection string and who will configure the database in#

--- RPokemonG/Services/FichaServices.cs
+++ RPokemonG/Services/FichaServices.cs
@@ -4,16 +4,16 @@ using RPokemonG.Models;
 
 namespace RPokemonG.Services
 {
-    public class FichaServices
+    public class DatabaseSetting
     {
         private readonly IMongoCollection<Ficha> _fichaCollection;//somente leitura - puxa a tabela e crio o nome
 
-        public FichaServices(IOptions<DatabaseSettings> fichaService)//Configurar algumas coisas do bd
+        public FichaServices(IOptions<DatabaseSettings> fichaService)//Configura database
         {
-            var mongoClient = new MongoClient(fichaService.Value.ConnectionString);
+            var mongoClient = new MongoClient(fichaService.Value.ConnectionString);//puxando o banco do appsettings
             var mongoDatabase = mongoClient.GetDatabase(fichaService.Value.DatabaseName);
 
-            _fichaCollection = mongoDatabase.GetCollection<Ficha>(fichaService.Value.FichaCollectionName);
+            _fichaCollection = mongoDatabase.GetCollection<Ficha>(databaseService.Value.FichaCollectionName);
         }
 
     }
