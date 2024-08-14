4224c32f4f085cc57499e56d15b0a1c8d23be377
DaviBenassiTs <davibenassiofc@gmail.com>
2024-08-13 21:23:46 -0300

    Create the methods

--- RPokemonG/Services/FichaServices.cs
+++ RPokemonG/Services/FichaServices.cs
@@ -8,13 +8,25 @@ namespace RPokemonG.Services
     {
         private readonly IMongoCollection<Ficha> _fichaCollection;//somente leitura - puxa a tabela e crio o nome
 
-        public FichaServices(IOptions<DatabaseSettings> fichaService)//Configurar algumas coisas do bd
+        public FichaServices(IOptions<DatabaseSettings> fichaService)//Configura database
         {
             var mongoClient = new MongoClient(fichaService.Value.ConnectionString);
             var mongoDatabase = mongoClient.GetDatabase(fichaService.Value.DatabaseName);
 
             _fichaCollection = mongoDatabase.GetCollection<Ficha>(fichaService.Value.FichaCollectionName);
         }
+
+        public async Task<List<Ficha>> GetAsync() =>
+            await _fichaCollection.Find(x => true).ToListAsync();
+        public async Task<Ficha> GetAsync(string id) =>
+            await _fichaCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        public async Task CreateAsync (Ficha ficha) =>
+            await _fichaCollection.InsertOneAsync(ficha);
+        public async Task UpdateAsync (string id, Ficha ficha) =>
+            await _fichaCollection.ReplaceOneAsync(x => x.Id == id, ficha);
+        public async Task RemoveAsync(string id) => 
+            await _fichaCollection.DeleteOneAsync(x => x.Id == id);
+        //criando os metodos 
 
     }
 }
