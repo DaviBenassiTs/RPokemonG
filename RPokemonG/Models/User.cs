2eef68657089e09b514b68c5ce4554c6447671c8
DaviBenassiTs <davibenassiofc@gmail.com>
2024-08-22 21:53:02 -0300

    Create the SignalR structure

--- RPokemonG/Models/User.cs
+++ RPokemonG/Models/User.cs
@@ -10,22 +10,6 @@ namespace RPokemonG.Models
         public string? Id { get; private set; } //private pra conseguir apenas colocar as informações pelo construtor
         public string Nome { get; private set; }
         public string Email { get; private set; }
-        public byte[] PasswordHash { get; private set; } //cria um hash da senha para não ter a senha pura no banco
-        public byte[] PasswordSalt { get; private set; }
-
-        public User(string nome, string email)
-        {
-            Nome = nome;    
-            Email = email;
-        }
-        public void EncryptPassword(byte[] passwordHash, byte[] passwordSalt)
-        {
-            PasswordHash = passwordHash;
-            PasswordSalt = passwordSalt;
-        }
-        private void ValidateDomain()
-        {
-           
-        }
+        public string Senha { get; private set; } 
     }
 }
