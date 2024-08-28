e73dcc478da5e43284e966f84f32515971543bbd
DaviBenassiTs <davibenassiofc@gmail.com>
2024-08-27 23:55:38 -0300

    Encapsulate the builders lines

---
+++ RPokemonG/Builders/CorsBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace RPokemonG.Builders
+{
+    public static class CorsBuilder
+    {
+        public static void UseCorsApp(this WebApplication app)
+        {
+            app.UseCors(
+            builder =>
+            {
+                builder.SetIsOriginAllowed(origin => true);
+                builder.AllowCredentials();
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+    });
+        }
+    }
+}
--- RPokemonG/Models/User.cs
+++ RPokemonG/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
 
 namespace RPokemonG.Models
 {
@@ -8,8 +9,11 @@ namespace RPokemonG.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; private set; } //private pra conseguir apenas colocar as informações pelo construtor
+        [StringLength(16, ErrorMessage = "Name length can't be more than 16.", MinimumLength = 4)]
         public string Nome { get; private set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; private set; }
+        [DataType(DataType.Password)]
         public string Senha { get; private set; } 
     }
 }
